292580d3482d212f573f024c43df111d623a8b28
Krittamet08 <goto08820@gamil.com>
2025-04-03 22:14:09 +0700

    fook

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'computing.psu.ac.th',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
